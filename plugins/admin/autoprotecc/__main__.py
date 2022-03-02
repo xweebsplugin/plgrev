0e19ce7006ccf3e8b36b7435608a6ff17cf2046c
xweebsplugin <100774525+xweebsplugin@users.noreply.github.com>
2022-03-02 18:02:37 +0530

    Update __main__.py

--- plugins/admin/autoprotecc/__main__.py
+++ plugins/admin/autoprotecc/__main__.py
@@ -3,7 +3,7 @@ import requests
 from bs4 import BeautifulSoup
 import asyncio
 from userge.utils import take_screen_shot
-from userge import userge, Message, Config, filters
+from userge import userge, Message, config, filters
 
 from PIL import Image
 import imagehash
@@ -75,9 +75,9 @@ async def dbrev_search_img(message: Message):
         if message_.photo:
             dis = await message.client.download_media(
                 message=message_,
-                file_name=Config.DOWN_PATH
+                file_name=config.DOWN_PATH
             )
-            dis_loc = os.path.join(Config.DOWN_PATH, os.path.basename(dis))
+            dis_loc = os.path.join(config.DOWN_PATH, os.path.basename(dis))
         if dis_loc:
             hash = str(imagehash.average_hash(Image.open(dis_loc)))
             c2db = dbhashall.find_one({hash:{'$exists': 1}})
